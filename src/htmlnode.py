896321df6b9ed20d569efc6940fc8d262f7cff37
BaSche <15978503+BaSche@users.noreply.github.com>
2024-05-23 17:49:44 +0200

    HTMLNOde init and props_to_HTML

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,3 +1,27 @@
 class HTMLNode:
-    def __init__(self):
-        pass
+    def __init__(self,
+            tag=None,
+            value=None,
+            children=None,
+            props=None
+            ):
+
+        if not value and not children:
+            raise ValueError("either value or children are needed")
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def props_to_html(self):
+        propsAsHTML = list()
+        for p in self.props:
+            propsAsHTML.append(f'{p}="{self.props[p]}"')
+        return " ".join(propsAsHTML)
+
+    def to_html(self):
+        raise NotImplementedError
+
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value})"
+
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -3,7 +3,23 @@ from htmlnode import HTMLNode
 
 class Test_HTMLNode(unittest.TestCase):
 
-    
+    def test_init_AssertValueOrChildren(self):
+        with self.assertRaises(ValueError, msg = "ValueError expected iof value and children are None"):
+            HTMLNode(None, None, None, None) 
+
+        with self.assertRaises(ValueError, msg = "ValueError expected if no value and children are empty"):
+            HTMLNode(None, "", (), None)
+
     def test_repr(self):
         node = HTMLNode("tag", "value", (), {})
-            
+        self.assertEqual(
+                "HTMLNode(tag, value)",
+                "" + node.__repr__())
+
+    def test_props_to_html_EmptyProps(self):
+        node = HTMLNode("tag", "value", None, {})
+        self.assertEqual("", node.props_to_html(), "empty props must be rendered as empty string")
+
+    def test_props_to_html(self):
+        node = HTMLNode("tag", "value", None, {"p1" : "v1", "p2" : "v2"})
+        self.assertEqual('p1="v1" p2="v2"', node.props_to_html())
